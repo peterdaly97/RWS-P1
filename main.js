96129425d4863649de8ccff9ba6d0ed82b50f96c
Peter Daly <peterd060397@gmail.com>
2018-10-31 10:48:18 +0000

    First Commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/**
+ *main is the entry point in Java
+ *
+ */
+
+
+function main()
+{
+	this.input = new Input();
+}
+
+function getKeys(key)
+{
+	console.log(key);
+}
+
+function getMouse(mousePos, direction)
+{
+	console.log("X: " + mousePos.x + ", Y: " + mousePos.y);
+	console.log(direction);
+}
